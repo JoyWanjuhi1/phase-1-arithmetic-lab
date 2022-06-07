b5bded95c8eaac9c2645ff3e52a9658c09e136ab
Joy Wanjuhi <joywanjuhi@gmail.com>
2022-06-07 09:41:22 +0300

    commit tree 16a60b637f7a1daf498b1b8cd4d013aae9b68d80

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+const num1=31 ; 
+const num2=2 ;
+
+const multiply = num1 * num2 ;
+
+const random = +1;
+
+const num3=29;
+const num4=5;
+
+const mod = num3 % num4 ;
+
+const max= Math.max(1,2,3,4,5,6,7,8,9,10,15,18,20);
+
+
+
+
+
+
